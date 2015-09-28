628c1ca87c5752963d68fbe90a57475ca1e8e409
ohhepicfail <ohh.epicfail@gmail.com>
2015-09-28 21:18:29 +0300

    lecture 2
    some notes

---
+++ strlen/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+unsigned long long strlen (const char str[]);
+
+int main ()
+{
+    char *str;
+    char c;
+    int i = 0;
+    gets (str);
+
+    printf ("size str = %llu\n", strlen(str));
+
+    return 0;
+}
+
+unsigned long long strlen (const char str[])
+{
+    assert (str);
+
+    unsigned long long counter = 0;
+
+    while (*str++ != '\0')
+    {
+        ++counter;
+    }
+
+    return counter;
+}
